bae2d02336386570aca39f8c55f7e49ac5cb4086
pratham0045jain <deepakjain9027@gmail.com>
2020-07-10 09:48:51 +0530

    i have done

--- sketch.js
+++ sketch.js
@@ -1,9 +1,36 @@
+
+var backgroundImg;
+
+var drawing = [];
+
+function preload() {
+  backgroundImg = loadImage("background1.png");
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+ // createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(backgroundImg)  
+  //drawSprites();
+
+  point = {
+    x : mouseX,
+    y : mouseY
+  }
+
+  drawing.push(point);
+
+  fill(255);
+  stroke("red");
+  beginShape();
+  for (var j = 0; j<drawing.length;j++){
+    vertex(drawing[j].x,drawing[j].y)
+    
+  }
+  endShape();
+  //drawing = [];
+  
 }
