8c82dddb46ae39650c22809b7ecfcf386b20b036
pratham0045jain <deepakjain9027@gmail.com>
2021-06-19 18:03:49 +0530

    i have done this

--- sketch.js
+++ sketch.js
@@ -1,41 +1,128 @@
-
-var backgroundImg;
-
+var database;
 var drawing = [];
+var currentPath = [];
+var backgroundImg;
+var isDrawing = false;
 
 function preload() {
   backgroundImg = loadImage("background1.png");
 }
 
 function setup() {
+  canvas = createCanvas(displayWidth, displayHeight - 350);
+  canvas.mousePressed(startPath);
+  canvas.parent("canvascontainer");
+  canvas.mouseReleased(endPath);
+
   
-  createCanvas(1900,900);
- // createSprite(400, 200, 50, 50);
+
+  database = firebase.database();
+  var ref = database.ref("drawings");
+  ref.on("value", gotData, errData);
+
+  var saveButton = select("#saveButton");
+  saveButton.mousePressed(saveDrawing);
+
+  /* var clearButton = select("#clearButton");
+  clearButton.mousePressed(clearDrawing); */
+
+
+
+
+}
+
+function startPath() {
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function endPath() {
+  isDrawing = false;
 }
 
 function draw() {
-  
-  background(backgroundImg)  
-  
-  //drawSprites();
+  background(backgroundImg);
 
-  point = {
-    x : mouseX,
-    y : mouseY
+  if (isDrawing) {
+    var point = {
+      x: mouseX,
+      y: mouseY
+    }
+    currentPath.push(point);
   }
 
-  drawing.push(point);
-  
-  fill(255);
-  stroke("red");
-  beginShape();
-  for (var j = 0; j<drawing.length;j++){
-    vertex(drawing[j].x,drawing[j].y)
-    
+
+  stroke(0);
+  strokeWeight(4);
+  noFill();
+  for (var i = 0; i < drawing.length; i++) {
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++) {
+      vertex(path[j].x, path[j].y)
+    }
+    endShape();
   }
-  noFill()
-  endShape();
-  
-  //drawing = [];
-  
 }
+
+function saveDrawing() {
+  var ref = database.ref("drawings");
+  var data = {
+    name: "Pratham",
+    drawing: drawing
+  }
+  var result = ref.push(data, dataSent);
+  console.log(result.key);
+
+
+  function dataSent(err, status) {
+
+  }
+
+}
+
+function gotData(data) {
+
+  //clear the listing
+  var elts = selectAll('.listing');
+  for (var i = 0; i < elts.length; i++){
+    elts[i].remove();
+  }
+
+
+
+  var drawings = data.val();
+  var keys = Object.keys(drawings);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    //console.log(key);
+
+    var li = createElement('li','');
+    li.class('listing');
+    var ahref = createA("#",key);
+    ahref.mousePressed(showDrawing);
+    ahref.parent(li);
+    li.parent('drawinglist');
+  }
+}
+
+function errData(err) {
+  console.log(err);
+}
+
+function showDrawing(){
+  var key = this.html()
+  var ref = database.ref('drawings/' + key);
+  ref.once('value', oneDrawing, errData);
+
+  function oneDrawing(data){
+    var dbDrawing = data.val();
+    drawing = dbDrawing.drawing;
+    //console.log(drawing);
+  }
+}
+
+/* function clearDrawing(){
+  drawing = [];
+} */
